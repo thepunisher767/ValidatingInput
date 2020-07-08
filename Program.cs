668f13e12bef67ee4879d7be89e1887e96019059
zscholts <zachscholts@quickenloans.com>
2020-07-08 17:59:33 -0400

    Changed over to methods

--- Program.cs
+++ Program.cs
@@ -15,62 +15,22 @@ namespace ValidatingInput
             {
                 Console.Clear();
 
-                Regex emailRegex = new Regex(@"^[a-zA-Z0-9]{5,30}@[a-zA-Z0-9]{5,10}\.[a-zA-Z0-9]{2,3}$");
-                Regex nameRegex = new Regex(@"^[A-Z]{1}[a-zA-Z]{0,29}$");
-                Regex dateRegex = new Regex(@"^([1-9]|[0-2][0-9]|3[0-1])[/]([1-9]|1[012]|0[1-9])[/][0-9]{4}$");
-                Regex phoneRegex = new Regex(@"^[(]{0,1}[0-9]{3}[)]{0,1}[-]{0,1}[.]{0,1}[0-9]{3}[-]{0,1}[.]{0,1}[0-9]{4}$");
-
                 Console.Write($"\nPlease enter a valid Name: ");
-                string testname = Console.ReadLine();
-                bool matchNameRegex = nameRegex.IsMatch(testname);
-
-                if (matchNameRegex)
-                {
-                    Console.WriteLine("Name is valid!");
-                }
-                else
-                {
-                    Console.WriteLine("Sorry, name is not valid!");
-                }
+                var nameResult = NameRegex(Console.ReadLine());
+                Console.WriteLine(nameResult);
 
                 Console.Write($"\nPlease enter a valid Email: ");
-                string testemail = Console.ReadLine();
-                bool matchEmailRegex = emailRegex.IsMatch(testemail);
-
-                if (matchEmailRegex)
-                {
-                    Console.WriteLine("Email is valid!");
-                }
-                else
-                {
-                    Console.WriteLine("Sorry, email is not valid!");
-                }
+                var emailResult = EmailRegex(Console.ReadLine());
+                Console.WriteLine(emailResult);
 
                 Console.Write($"\nPlease enter a valid phone number: ");
-                string testphone = Console.ReadLine();
-                bool matchPhoneRegex = phoneRegex.IsMatch(testphone);
+                var phoneResult = PhoneRegex(Console.ReadLine());
+                Console.WriteLine(phoneResult);
 
-                if (matchPhoneRegex)
-                {
-                    Console.WriteLine("Phone is valid!");
-                }
-                else
-                {
-                    Console.WriteLine("Sorry, phone is not valid!");
-                }
-                
                 Console.Write($"\nPlease enter a valid Date: ");
-                string testdate = Console.ReadLine();
-                bool matchDateRegex = dateRegex.IsMatch(testdate);
+                var dateResult = DateRegex(Console.ReadLine());
+                Console.WriteLine(dateResult);
 
-                if (matchDateRegex)
-                {
-                    Console.WriteLine("Date is valid!");
-                }
-                else
-                {
-                    Console.WriteLine("Sorry, date is not valid!");
-                }
 
                 Console.Write($"\nWould you like to try again?? (q to Quit): ");
                 string userInput = Console.ReadLine();
@@ -83,6 +43,74 @@ namespace ValidatingInput
             }
 
             Console.WriteLine($"\nGood Bye!!!!!!!!!!!");
+        }
+
+        public static string NameRegex(string testname)
+        {
+            string answer;
+            Regex nameRegex = new Regex(@"^[A-Z]{1}[a-zA-Z]{0,29}$");
+            bool matchNameRegex = nameRegex.IsMatch(testname);
+
+            if (matchNameRegex)
+            {
+                answer = "Name is valid!";
+            }
+            else
+            {
+                answer = "Sorry, name is not valid!";
+            }
+            return answer;
+        }
+
+        public static string EmailRegex(string testemail)
+        {
+            string answer;
+            Regex emailRegex = new Regex(@"^[a-zA-Z0-9]{5,30}@[a-zA-Z0-9]{5,10}\.[a-zA-Z0-9]{2,3}$");
+            bool matchEmailRegex = emailRegex.IsMatch(testemail);
+
+            if (matchEmailRegex)
+            {
+                answer = "Email is valid!";
+            }
+            else
+            {
+                answer = "Sorry, email is not valid!";
+            }
+            return answer;
+        }
+
+        public static string PhoneRegex(string testphone)
+        {
+            string answer;
+            Regex phoneRegex = new Regex(@"^[(]{0,1}[0-9]{3}[)]{0,1}[-]{0,1}[.]{0,1}[0-9]{3}[-]{0,1}[.]{0,1}[0-9]{4}$");
+            bool matchPhoneRegex = phoneRegex.IsMatch(testphone);
+
+            if (matchPhoneRegex)
+            {
+                answer = "Phone is valid!";
+            }
+            else
+            {
+                answer = "Sorry, phone is not valid!";
+            }
+            return answer;
+        }
+
+        public static string DateRegex(string testdate)
+        {
+            string answer;
+            Regex dateRegex = new Regex(@"^([1-9]|[0-2][0-9]|3[0-1])[/]([1-9]|1[012]|0[1-9])[/][0-9]{4}$");
+            bool matchDateRegex = dateRegex.IsMatch(testdate);
+
+            if (matchDateRegex)
+            {
+                answer = "Date is valid!";
+            }
+            else
+            {
+                answer = "Sorry, date is not valid!";
+            }
+            return answer;
         }
     }
 }
