ae072c963aa18f0bc7eb35fa43761d584f377493
zscholts <zachscholts@quickenloans.com>
2020-07-09 11:18:52 -0400

    General code cleanup. Changed method return types/fixed main loop

--- Program.cs
+++ Program.cs
@@ -19,21 +19,17 @@ namespace ValidatingInput
             {
                 Console.Clear();
 
-                Console.Write($"\nPlease enter a valid Name: ");
-                var nameResult = NameRegex(Console.ReadLine());
-                Console.WriteLine(nameResult);
+                Console.Write($"Please enter a valid Name: ");
+                NameRegex(Console.ReadLine());
 
                 Console.Write($"\nPlease enter a valid Email: ");
-                var emailResult = EmailRegex(Console.ReadLine());
-                Console.WriteLine(emailResult);
+                EmailRegex(Console.ReadLine());
 
                 Console.Write($"\nPlease enter a valid phone number: ");
-                var phoneResult = PhoneRegex(Console.ReadLine());
-                Console.WriteLine(phoneResult);
+                PhoneRegex(Console.ReadLine());
 
                 Console.Write($"\nPlease enter a valid Date: ");
-                var dateResult = DateRegex(Console.ReadLine());
-                Console.WriteLine(dateResult);
+                DateRegex(Console.ReadLine());
 
 
                 Console.Write($"\nWould you like to try again?? (q to Quit): ");
@@ -48,72 +44,64 @@ namespace ValidatingInput
             Console.WriteLine($"\nGood Bye!!!!!!!!!!!");
         }
 
-        public static string NameRegex(string testname)
+        public static void NameRegex(string testname)
         {
-            string answer;
             Regex nameRegex = new Regex(@"^[A-Z]{1}[a-zA-Z]{0,29}$");
             bool matchNameRegex = nameRegex.IsMatch(testname);
 
             if (matchNameRegex)
             {
-                answer = "Name is valid!";
+                Console.WriteLine("Name is valid!");
             }
             else
             {
-                answer = "Sorry, name is not valid!";
+                Console.WriteLine("Sorry, name is not valid!");
             }
-            return answer;
         }
 
-        public static string EmailRegex(string testemail)
+        public static void EmailRegex(string testemail)
         {
-            string answer;
             Regex emailRegex = new Regex(@"^[a-zA-Z0-9]{5,30}@[a-zA-Z0-9]{5,10}\.[a-zA-Z0-9]{2,3}$");
             bool matchEmailRegex = emailRegex.IsMatch(testemail);
 
             if (matchEmailRegex)
             {
-                answer = "Email is valid!";
+                Console.WriteLine("Email is valid!");
             }
             else
             {
-                answer = "Sorry, email is not valid!";
+                Console.WriteLine("Sorry, email is not valid!");
             }
-            return answer;
         }
 
-        public static string PhoneRegex(string testphone)
+        public static void PhoneRegex(string testphone)
         {
-            string answer;
             Regex phoneRegex = new Regex(@"^\({0,1}[0-9]{3}\){0,1}-{0,1}\.{0,1}[0-9]{3}-{0,1}\.{0,1}[0-9]{4}$");
             bool matchPhoneRegex = phoneRegex.IsMatch(testphone);
 
             if (matchPhoneRegex)
             {
-                answer = "Phone is valid!";
+                Console.WriteLine("Phone is valid!");
             }
             else
             {
-                answer = "Sorry, phone is not valid!";
+                Console.WriteLine("Sorry, phone is not valid!");
             }
-            return answer;
         }
 
-        public static string DateRegex(string testdate)
+        public static void DateRegex(string testdate)
         {
-            string answer;
             Regex dateRegex = new Regex(@"^([1-9]|[0-2][0-9]|3[0-1])/([1-9]|1[012]|0[1-9])/[0-9]{4}$");
             bool matchDateRegex = dateRegex.IsMatch(testdate);
 
             if (matchDateRegex)
             {
-                answer = "Date is valid!";
+                Console.WriteLine("Date is valid!");
             }
             else
             {
-                answer = "Sorry, date is not valid!";
+                Console.WriteLine("Sorry, date is not valid!");
             }
-            return answer;
         }
     }
 }
